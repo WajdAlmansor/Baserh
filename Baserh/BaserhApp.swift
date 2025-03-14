c414b137b9957ac45c25bd918506a1dbfc8d82e5
wajdAlmansor <wajd2828@gmail.com>
2025-03-14 14:20:32 +0300

    Initial Commit

---
+++ Baserh/BaserhApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaserhApp.swift
+//  Baserh
+//
+//  Created by Wajd on 14/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct BaserhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
